a8fcf9e1148fdd7e6ac933e1c939a3b5ae10fb1f
Life4gal <Life4gal@gmail.com>
2022-09-20 14:27:08 +0800

    `feat`: Reformat the unit test.✨

---
+++ unit_test/src/dummy_test.cpp
@@ -0,0 +1,44 @@
+#include <boost/ut.hpp>
+#include <tuple>
+
+using namespace boost::ut;
+
+suite test_parameterized = []
+{
+	for (auto i: std::vector{1, 2, 3})
+	{
+		test("parameterized " + std::to_string(i)) = [i]
+		{
+			expect(that % i > 0);
+		};
+	}
+};
+
+suite test_args_and_types = []
+{
+	"args"_test = [](auto arg)
+	{
+		expect(arg >= 1_i);
+	} | std::vector{1, 2, 3};
+
+	"types"_test = []<typename T>
+	{
+		expect(std::is_integral_v<T>) << "all types are integrals";
+	} | std::tuple<bool, int>{};
+
+	"args and types"_test = []<typename T>(T arg)
+	{
+		expect(std::is_integral_v<T> >> fatal);
+		// todo: ambiguous?
+		// expect(42_i == arg or "is true"_b == arg);
+		expect(type<T> == type<int> or type<T> == type<bool>);
+		if constexpr (type<T> == type<int>)
+		{
+			expect(42_i == arg);
+		}
+		else
+		{
+			expect("is true"_b == arg);
+		}
+	} | std::tuple{true, 42};
+};
---
+++ unit_test/src/test_ctp.cpp
@@ -0,0 +1,18 @@
+#include <CMakeTemplateProject/macro.hpp>
+#include <CMakeTemplateProject/hello.hpp>
+#include <boost/ut.hpp>
+
+CTP_DISABLE_WARNING_PUSH
+CTP_DISABLE_WARNING(-Wswitch-enum)
+
+// :(
+#include <fmt/format.h>
+
+CTP_DISABLE_WARNING_POP
+
+using namespace boost::ut;
+
+suite answer_test = []{
+	expect(type<decltype(ctp::answer())> == type<int>) << fmt::format("Expected type of answer is `{}`, but actually `{}`.", reflection::type_name<int>(), reflection::type_name<decltype(ctp::answer())>());
+	expect(ctp::answer() == 42_i) << "No! The answer should be 42!";
+};
