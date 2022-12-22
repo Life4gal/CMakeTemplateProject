ff1097a646cece7eb488dd23f02b54cc13238f43
life4gal <life4gal@gmail.com>
2022-12-22 10:42:10 +0800

    GitHub action🎉

--- cmake/compile_options_setter.cmake
+++ cmake/compile_options_setter.cmake
@@ -187,19 +187,21 @@ function(set_compile_property target_name)
 			CXX_STANDARD_REQUIRED TRUE
 	)
 
-	if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
-		set_target_properties(
-				${target_name}
-				PROPERTIES
-				COMPILE_OPTIONS "/W4;/WX"
-		)
-	elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-		set_target_properties(
-				${target_name}
-				PROPERTIES
-				COMPILE_OPTIONS "-Wall;-Wextra;-Wpedantic;-Werror"
-		)
-	else()
-		message("Unknown Compiler ID: ${CMAKE_CXX_COMPILER_ID}")
-	endif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+	if(NOT GITHUB_WORKFLOWS)
+		if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+			set_target_properties(
+					${target_name}
+					PROPERTIES
+					COMPILE_OPTIONS "/W4;/WX"
+			)
+		elseif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
+			set_target_properties(
+					${target_name}
+					PROPERTIES
+					COMPILE_OPTIONS "-Wall;-Wextra;-Wpedantic;-Werror"
+			)
+		else()
+			message("Unknown Compiler ID: ${CMAKE_CXX_COMPILER_ID}")
+		endif("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+	endif(NOT GITHUB_WORKFLOWS)
 endfunction(set_compile_property)
