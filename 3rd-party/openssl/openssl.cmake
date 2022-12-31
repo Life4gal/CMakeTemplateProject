0c82cc5f3403780de1d774b1785365278291e710
life4gal <life4gal@gmail.com>
2022-12-31 17:11:42 +0800

    fix: #1 `It seems that the openssl installed by NuGet is not found by curl`

--- 3rd-party/openssl/openssl.cmake
+++ 3rd-party/openssl/openssl.cmake
@@ -3,8 +3,23 @@ function(install_openssl_windows)
 	set(LIB_OPENSSL_VERSION 1.1.0)
 
 	set(LIB_OPENSSL_PATH ${CMAKE_BINARY_DIR}/packages/${LIB_OPENSSL_NAME}.${LIB_OPENSSL_VERSION}/build/native)
+	set(LIB_OPENSSL_PATH ${CMAKE_BINARY_DIR}/packages/${LIB_OPENSSL_NAME}.${LIB_OPENSSL_VERSION}/build/native PARENT_SCOPE)
 
 	nuget_install(LIB_OPENSSL ${${PROJECT_NAME_PREFIX}3RD_PARTY_PATH}/openssl/openssl.config.in)
+
+	add_library(OpenSSL::Crypto SHARED IMPORTED)
+	set_target_properties(
+			OpenSSL::Crypto PROPERTIES
+			IMPORTED_LOCATION ${LIB_OPENSSL_PATH}/bin/libcrypto-vc142-x64-1_1_0.dll
+			IMPORTED_IMPLIB ${LIB_OPENSSL_PATH}/bin/libcrypto-vc142-x64-1_1_0.lib
+	)
+
+	add_library(OpenSSL::SSL SHARED IMPORTED)
+	set_target_properties(
+			OpenSSL::SSL PROPERTIES
+			IMPORTED_LOCATION ${LIB_OPENSSL_PATH}/bin/libssl-vc142-x64-1_1_0.dll
+			IMPORTED_IMPLIB ${LIB_OPENSSL_PATH}/bin/libssl-vc142-x64-1_1_0.lib
+	)
 
 	target_include_directories(
 			${PROJECT_NAME}
@@ -15,22 +30,26 @@ function(install_openssl_windows)
 	target_link_libraries(
 			${PROJECT_NAME}
 			PRIVATE
-			${LIB_OPENSSL_PATH}/bin/libssl-vc142-x64-1_1_0.lib
-			${LIB_OPENSSL_PATH}/bin/libcrypto-vc142-x64-1_1_0.lib
+			#${LIB_OPENSSL_PATH}/bin/libssl-vc142-x64-1_1_0.lib
+			#${LIB_OPENSSL_PATH}/bin/libcrypto-vc142-x64-1_1_0.lib
+
+			OpenSSL::SSL
+			OpenSSL::Crypto
 	)
 	configure_file(
 			${LIB_OPENSSL_PATH}/bin/libssl-vc142-x64-1_1_0.dll
-			${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/libssl-vc142-x64-1_1_0.dll
+			#${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/libssl-vc142-x64-1_1_0.dll
+			${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/libssl-1_1-x64.dll
 			COPYONLY
 	)
 	configure_file(
 			${LIB_OPENSSL_PATH}/bin/libcrypto-vc142-x64-1_1_0.dll
-			${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/libcrypto-vc142-x64-1_1_0.dll
+			#${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/libcrypto-vc142-x64-1_1_0.dll
+			${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/libcrypto-1_1-x64.dll
 			COPYONLY
 	)
 
-	list(APPEND ${PROJECT_NAME_PREFIX}3RD_PARTY_DEPENDENCIES "${LIB_OPENSSL_NAME} ${LIB_OPENSSL_VERSION}")
-	set(${PROJECT_NAME_PREFIX}3RD_PARTY_DEPENDENCIES ${${PROJECT_NAME_PREFIX}3RD_PARTY_DEPENDENCIES} PARENT_SCOPE)
+	set(${PROJECT_NAME_PREFIX}3RD_PARTY_DEPENDENCIES ${${PROJECT_NAME_PREFIX}3RD_PARTY_DEPENDENCIES} "${LIB_OPENSSL_NAME} ${LIB_OPENSSL_VERSION}" PARENT_SCOPE)
 endfunction(install_openssl_windows)
 
 function(install_openssl_linux)
@@ -55,8 +74,7 @@ function(install_openssl_linux)
 			${LIB_OPENSSL_LIBRARIES}
 	)
 
-	list(APPEND ${PROJECT_NAME_PREFIX}3RD_PARTY_DEPENDENCIES "openssl ${LIB_OPENSSL_VERSION}")
-	set(${PROJECT_NAME_PREFIX}3RD_PARTY_DEPENDENCIES ${${PROJECT_NAME_PREFIX}3RD_PARTY_DEPENDENCIES} PARENT_SCOPE)
+	set(${PROJECT_NAME_PREFIX}3RD_PARTY_DEPENDENCIES ${${PROJECT_NAME_PREFIX}3RD_PARTY_DEPENDENCIES} "openssl ${LIB_OPENSSL_VERSION}" PARENT_SCOPE)
 endfunction(install_openssl_linux)
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
@@ -85,6 +103,8 @@ elseif (CMAKE_CXX_COMPILER_ID MATCHES "AppleClang")
 	#########################
 	# CLANG
 	########################
+	# todo
+	install_openssl_linux()
 else ()
 	message(FATAL_ERROR "Unsupported compilers: ${CMAKE_CXX_COMPILER}")
 endif (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
