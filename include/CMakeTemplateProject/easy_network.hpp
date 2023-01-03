b62c9e031f27cb971faa72be52753fa78aaec748
life4gal <life4gal@gmail.com>
2023-01-03 10:07:36 +0800

    Fix the linking problem caused by EasyNetwork not exported on Windows platform compilation.

--- include/CMakeTemplateProject/easy_network.hpp
+++ include/CMakeTemplateProject/easy_network.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <CMakeTemplateProject/macro.hpp>
 #include <string>
 #include <string_view>
 
 namespace ctp
 {
-	class EasyNetwork
+	class CTP_EXPORTED_SYMBOL EasyNetwork
 	{
 	public:
 		[[nodiscard]] static auto https_support_check() -> bool;
